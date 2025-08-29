6b5b4289eeb5e7b88685ba5a9fc19ad1fd4fa5e0
eunwoo031217 <eunwoo031217@gmail.com>
2025-08-30 04:21:25 +0900

    feat: 검색페이지 서버

--- app/src/main/java/com/appcenter/uniclub/data/SearchRepository.kt
+++ app/src/main/java/com/appcenter/uniclub/data/SearchRepository.kt
@@ -1,2 +1,10 @@
 package com.appcenter.uniclub.data
 
+import com.appcenter.uniclub.network.SearchService
+import com.appcenter.uniclub.network.dto.ClubResponseDto
+
+class SearchRepository(private val service: SearchService) {
+    suspend fun searchClubs(keyword: String): List<ClubResponseDto> {
+        return service.searchClubs(keyword)
+    }
+}
